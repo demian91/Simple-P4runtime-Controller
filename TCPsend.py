97641149535fa0a4b69d88c959be70b544fb6b00
Belma T <belma.turkovic.1991@gmail.com>
2020-12-18 19:48:09 +0100

    TCPsend fix

--- TCPsend.py
+++ TCPsend.py
@@ -11,7 +11,7 @@ def main():
     #send(packet)
 
 
-def packet_with_seq_n(port, src_ip):
+def packet_with_seq_n():
     packet = Ether(src="00:04:00:00:00:00", dst="00:00:01:01:01:01")/IP(dst="10.10.3.3", src="10.10.10.1")/TCP(sport=7777, dport=7777, flags="S")/"111111112222222233333333"
     sendp(packet, iface="eth0")
 
@@ -20,5 +20,5 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Simple script that sends TCP packets to an interface using scapy package')
     args = parser.parse_args()
     for i in range(0, 1):
-        packet_with_seq_n(int(args.dst_port), args.src_ip)
+        packet_with_seq_n()
 
